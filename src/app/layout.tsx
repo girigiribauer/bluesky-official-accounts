8ae6e45036a98fd25ad336ece4c6e4586f16faed
girigiribauer <girigiribauer@gmail.com>
2024-12-19 12:45:26 +0900

    CSSの指定変更

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,6 @@ export default function RootLayout({
           sizes="16x16"
           href="/favicon-16x16.png"
         />
-        <link rel="stylesheet" type="text/css" href="/css/all.min.css" />
         <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body>
