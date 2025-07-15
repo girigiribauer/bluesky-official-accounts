ec2f7fff84aeef36a59ab5fbf45d20889adf22bb
girigiribauer <girigiribauer@gmail.com>
2025-07-15 14:32:14 +0900

    メッセージ追加

--- src/components/NewsList.tsx
+++ src/components/NewsList.tsx
@@ -11,6 +11,9 @@ export const NewsList = ({ items }: NewsListProps) => {
   return (
     <div className={styles.container}>
       <h2>更新情報</h2>
+      <p>
+        投稿されたアカウントは随時反映されています！全体の更新情報のみこちらをご覧ください！
+      </p>
 
       {items.length > 0 ? (
         <div className={styles.box}>
