9448d0271855a36aef483933b7093877a4c0954b
girigiribauer <girigiribauer@gmail.com>
2026-04-15 23:09:08 +0900

    OAuth検証

---
+++ src/app/.well-known/jwks.json/route.ts
@@ -0,0 +1,9 @@
+import { NextResponse } from "next/server";
+import { getOAuthClient } from "src/lib/oauthClient";
+
+export const dynamic = "force-dynamic";
+
+export async function GET() {
+  const client = await getOAuthClient();
+  return NextResponse.json(client.jwks);
+}
